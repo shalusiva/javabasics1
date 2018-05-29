667cf5cdd14ce4a9536a5ae4b46cb58b96165bc0
shalusiva <shalu161198@gmail.com>
2018-05-29 16:14:59 +0530

    data type

---
+++ src/com/basics1/DataTypeExample.java
@@ -0,0 +1,13 @@
+package com.basics1;
+
+public class DataTypeExample {
+	public static void main(String s[])
+	{
+int a=8,b=9,c=76;
+double d;
+d=a+b+c;
+System.out.println("the value of:"+d);
+d++;
+System.out.println("incremented value of d"+d);
+}
+}
