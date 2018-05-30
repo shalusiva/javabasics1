92b78760feb43a57ec00f1fc927cdb443aad27ef
shalusiva <shalu161198@gmail.com>
2018-05-30 12:12:25 +0530

    basicerroe3

---
+++ src/com/basics1/basicerror2.java
@@ -0,0 +1,16 @@
+package com.basics1;
+
+public class basicerror2 {
+	public static void main(String[] args)
+	{
+		int x = 1;
+		int total=0;
+		while ( x <= 10 )
+		{
+		total += x;
+		++x;
+		}
+	System.out.println(total);
+	}
+
+}
