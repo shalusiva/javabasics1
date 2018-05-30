df88ca6c2f5dfa5e79bab1fedad01e83926fb7d3
shalusiva <shalu161198@gmail.com>
2018-05-30 10:12:52 +0530

    basic example

---
+++ src/com/basics1/condition.java
@@ -0,0 +1,18 @@
+package com.basics1;
+
+public class condition {
+	public static void main(String[] arg)
+	{ 
+		int count = 1;
+
+			while ( count <= 10 )
+			{
+			 System.out.println( count % 2 == 1 ? "****" : "++++++++" );
+			 ++count;
+			 
+			
+			 } 
+
+	}
+
+}
