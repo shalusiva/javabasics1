ed7748df87907dcf91c2a30a85e7ef84039d78e3
shalusiva <shalu161198@gmail.com>
2018-05-30 12:13:27 +0530

    reference

---
+++ src/com/basics1/basicerror3.java
@@ -0,0 +1,13 @@
+package com.basics1;
+
+public class basicerror3 {
+	public static void main(String[] args)
+	{
+		int x=1;
+		int total=0;
+	 while ( x <= 100 )
+total += x;
+++x;
+System.out.println(total);
+}
+}
