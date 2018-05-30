491b82b891c352a5490537760b806c196d0beb4a
shalusiva <shalu161198@gmail.com>
2018-05-30 09:56:23 +0530

    basic ifelse code

---
+++ src/com/basics1/age.java
@@ -0,0 +1,13 @@
+package com.basics1;
+
+public class age {
+public static void main(String[] args) {
+int i=10;
+if(i<20) {
+	System.out.println("i is lesser than 20");
+}
+else {
+	System.out.println("i is greater than 20");
+}
+}
+}
