a78ed40af7c4fabfe0229efcf0657fa9e6301f50
shalusiva <shalu161198@gmail.com>
2018-05-30 12:21:31 +0530

    basicerror4

---
+++ src/com/basics1/basicerror4.java
@@ -0,0 +1,16 @@
+package com.basics1;
+
+public class basicerror4 {
+	public static void main(String[] args)
+	{
+		int y=9;
+	
+		while (y> 0 )
+		{
+		System.out.println( y );
+		++y;
+
+	}
+
+}
+}
