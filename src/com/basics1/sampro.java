008fa46b3d49b269c4dcb85bd8371fa391f4af60
shalusiva <shalu161198@gmail.com>
2018-05-30 17:20:00 +0530

    samplepro

---
+++ src/com/basics1/sampro.java
@@ -0,0 +1,19 @@
+package com.basics1;
+
+public class sampro {
+	public static void main(String[] args)
+	{
+		
+		for ( int i = 1; i <= 10; i++ )
+		{
+		for ( int j = 1; j <= 5; j++ )
+		System.out.print( '@' );
+
+		 System.out.println();
+		 } // end outer for
+		 } // end main
+		 
+
+
+
+	}
