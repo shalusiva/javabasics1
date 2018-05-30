d29bb9ed2a8a511d58f7f758e43097a18718c63c
shalusiva <shalu161198@gmail.com>
2018-05-30 11:00:21 +0530

    basicerror

---
+++ src/com/basics1/basicerror.java
@@ -0,0 +1,16 @@
+package com.basics1;
+
+public class basicerror {
+	public static void main(String[] arg)
+	{
+	int age=50;
+	if ( age >= 65 )
+	{
+	System.out.println("Age is greater than or equal to 65" );
+	}
+	else
+	{
+	System.out.println("Age is less than 65 ");
+	}
+	}
+}
