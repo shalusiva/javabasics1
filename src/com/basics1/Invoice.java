29647253a6987617c9c26654225239d44a714cf3
shalusiva <shalu161198@gmail.com>
2018-05-31 14:43:09 +0530

    Invoices.java

--- src/com/basics1/Invoice.java
+++ src/com/basics1/Invoice.java
@@ -17,10 +17,10 @@ public String getPartDescription() {
 public void setPartDescription(String partDescription) {
 	this.partDescription = partDescription;
 }
-public int getquantity() {
+public int getQuantity() {
 	return quantity;
 }
-public void setquantity(int quantity) {
+public void setQuantity(int quantity) {
 	if(quantity<0) {
 		this.quantity=0;
 }else
@@ -29,14 +29,16 @@ public void setquantity(int quantity) {
 	this.quantity=quantity;
 }
 }
-public void setQuantity(int quantity) {
-	this.quantity = quantity;
-}
 
 public double getPrice() {
 	return price;
 }
 public void setPrice(double price) {
 	this.price = price;
+}
+public double getinvoiceAmount()
+{
+	double invoiceAmount=price*quantity;
+	return invoiceAmount;
 }
 }
--- src/com/basics1/Invoices.java
+++ src/com/basics1/Invoices.java
@@ -3,7 +3,13 @@ package com.basics1;
 public class Invoices {
 	public static void main(String[] args)
 	{
-		
+		Invoice invoice1=new Invoice();
+		 invoice1.setQuantity(5);
+		 invoice1.setPrice(100);
+		invoice1.setPartDescription("bolt");
+		 System.out.println(invoice1.getQuantity());
+		 double a= invoice1.getinvoiceAmount();
+		 System.out.println(invoice1.getinvoiceAmount());
 	}
 
 }
